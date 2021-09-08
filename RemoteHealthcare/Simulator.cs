6788d8124fcb396049ddc3a8b6f4fc95216953e0
Ewout <we.baars@student.avans.nl>
2021-09-08 10:56:59 +0200

    Refactor Simulator

--- RemoteHealthcare/Simulator.cs
+++ RemoteHealthcare/Simulator.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Threading;
-using RemoteHealthcare;
 
-namespace FietsDemo
+namespace RemoteHealthcare
 {
     class Simulator
     {
@@ -16,49 +15,69 @@ namespace FietsDemo
 
         public Simulator()
         {
-            Thread t = new Thread(new ThreadStart(run));
+            Thread t = new Thread(new ThreadStart(Run));
             t.Start();
             Console.ReadLine();
 
         }
-        public void run()
+        public void Run()
         {
             Random random = new Random();
             int i = 0;
             while (true)
             {
-                byte[] data = new byte[13];
-                data[0] = 0xA4;
-                data[1] = 0x09;
-                data[2] = 0x4E;
-                data[3] = 0x05;
-                data[4] = 0x10;
-                data[5] = 0x19;
-                data[6] = 0x61;
-                data[7] = 0x54;
-                i++;
-                double speed = 40 * (Math.Sin(i * 0.1) + 1) / 2;
-                short speedcalc = (short)(speed * 1000 * (1 / 3.6));
-
-                byte[] bytes = BitConverter.GetBytes(speedcalc);
-
-                data[8] = bytes[0];
-                data[9] = bytes[1];
-                data[10] = 0xFF;
-                data[11] = 0x24;
-                data[12] = 0x01;
-                //A4 09 4E 05 10 19 61 54 00 00 FF 24 01
+               
 
                 FakeBike fakeBike = new FakeBike();
-                fakeBike.Data = data;
+                fakeBike.Data = GenerateSpeedData(i);
                 Program.BleBike_SubscriptionValueChanged(fakeBike);
 
                 Thread.Sleep(1000);
 
+                i++;
             }
         }
-    }
 
+        private static byte[] GenerateSpeedData(int i)
+        {
+            byte[] data = generateAPage(0x10);
+           
+            double speed = 40 * (Math.Sin(i * 0.1) + 1) / 2;
+            short speedcalc = (short)(speed * 1000 * (1 / 3.6));
+
+            byte[] bytes = BitConverter.GetBytes(speedcalc);
+
+            data[8] = bytes[0];
+            data[9] = bytes[1];
+            
+            //A4 09 4E 05 10 19 61 54 00 00 FF 24 01
+
+            return data;
+        }
+
+        public static byte[] generateAPage(byte pagenumber)
+        {
+            byte[] data = new byte[13];
+            data[0] = 0xA4;
+            data[1] = 0x09;
+            data[2] = 0x4E;
+            data[3] = 0x05;
+            data[4] = pagenumber; //page number
+            data[5] = 0x19;
+            data[6] = 0x00;
+            data[7] = 0x00;
+            data[8] = 0x00;
+            data[9] = 0x00;
+            data[10] = 0xFF;
+            data[11] = 0x24;
+            data[12] = 0x01;
+
+            return data;
+        }
+
+
+
+    }
 
     class FakeBike : IBike
     {
