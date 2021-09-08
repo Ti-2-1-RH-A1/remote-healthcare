4cb9e10a7cc756d65477ac7c382db594da27e73f
Jeroen <j.visser10@student.avans.nl>
2021-09-08 14:45:52 +0200

    Added RealBike class.

---
+++ RemoteHealthcare/RealBike.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Avans.TI.BLE;
+
+namespace RemoteHealthcare
+{
+    class RealBike : BLE, IBike
+    {
+        public void SetAirResistance(byte Value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetResistance(byte Value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
