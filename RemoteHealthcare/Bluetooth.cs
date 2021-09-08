95117aa39bab4964844ac425f33b09f0eb01a548
Jeroen <j.visser10@student.avans.nl>
2021-09-08 15:22:09 +0200

    Added methods to RealBike class.

--- RemoteHealthcare/Bluetooth.cs
+++ RemoteHealthcare/Bluetooth.cs
@@ -13,7 +13,7 @@ namespace RemoteHealthcare
         static async Task MainBLE()
         {
             int errorCode = 0;
-            BLE bleBike = new BLE();
+            RealBike bleBike = new RealBike();
             BLE bleHeart = new BLE();
             Thread.Sleep(1000); // We need some time to list available devices
 
@@ -174,59 +174,6 @@ namespace RemoteHealthcare
             bytes[0] = byte1;
             bytes[1] = byte2;
             return BitConverter.ToUInt16(bytes, 0);
-        }
-
-        // byte resistance =- basic resistance in % (0x00 == 0% and 0xFF == 100%
-        public static void SetResistance(BLE bleBike, byte resistance)
-        {
-            byte datapage = 0x30;
-            byte zeroValue = 0x00;
-            byte[] payload = { datapage, zeroValue, zeroValue, zeroValue, zeroValue, zeroValue, zeroValue, resistance };
-            sendBluetoothMessage(bleBike, payload);
-        }
-
-        // byte airResistanceCoefficient = Coefficient of the air resistance input in % of total (0x00 = 0% and 0xFF = 100%), where actual coefficient goes from 0.00 to 1.86 kg/m
-        // byte windspeed = windspeed input in % of total (0x00 = 0% and 0xFF = 100%), where actual windspeed goes from -127 to 127 km/h
-        // byte draftingFactor = drafting factor input in % of total (0x00 = 0% and 0xFF = 100%), where actual factor goes from 0.0 to 1.00 (the lower this factor, the less air resistance matters)
-        public static void SetAirResistance(BLE bleBike, byte airResistanceCoefficient, byte windspeed, byte draftingFactor)
-        {
-            byte datapage = 0x32;
-            byte zeroValue = 0x00;
-            byte[] payload = { datapage, zeroValue, zeroValue, zeroValue, zeroValue, airResistanceCoefficient, windspeed, draftingFactor };
-            sendBluetoothMessage(bleBike, payload);
-        }
-
-        private static void sendBluetoothMessage(BLE bleBike, byte[] payload)
-        {
-            // Declare some standard values for the message.
-            byte sync = 0xA4;
-            byte length = 0x09;
-            byte msgId = 0x4E;
-            byte channelNumber = 0x05;
-
-            // Determine checksum
-            byte checksum = 0x00;
-            checksum ^= sync;
-            checksum ^= length;
-            checksum ^= msgId;
-            checksum ^= channelNumber;
-            foreach (byte b in payload)
-            {
-                checksum ^= b;
-            }
-
-            // length is payload + sync + length + msgId + channelnumber + checksum.
-            // So length is payload.Length + 5
-            byte[] data = new byte[payload.Length + 5];
-            data[0] = sync;
-            data[1] = length;
-            data[2] = msgId;
-            data[3] = channelNumber;
-            payload.CopyTo(data, 4);
-            data[data.Length - 1] = checksum;
-
-            Console.WriteLine("Trying to send byte array: " + string.Join(", ", data));
-            bleBike.WriteCharacteristic("6e40fec3-b5a3-f393-e0a9-e50e24dcca9e", data);
-        }
+        }        
     }
 }
