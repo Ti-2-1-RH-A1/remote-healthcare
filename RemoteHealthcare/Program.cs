3e9109e2856194db5fdbf43836b6c64fbc17a22a
Robin <luijtenrobin@gmail.com>
2021-09-15 15:35:25 +0200

    Static fix

--- RemoteHealthcare/Program.cs
+++ RemoteHealthcare/Program.cs
@@ -9,12 +9,12 @@ namespace RemoteHealthcare
         public SimulatorBike simulator = new SimulatorBike();
         public BikeManager bikeManager = new BikeManager();
         public HRManager hrManager = new HRManager();
+
         static void Main(string[] args)
         {
             ConsoleGUI cGUI = new ConsoleGUI();
-            cGUI.SelectionHandler(this);
+            Program program = new Program();
+            cGUI.SelectionHandler(program);
         }
-
-
     }
 }
