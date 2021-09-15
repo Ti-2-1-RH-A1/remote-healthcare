c5fd7db75866bfb321e2709f689211350c07329c
medkam <med.dekam@student.avans.nl>
2021-09-15 16:12:50 +0200

    Connection to vr sim client possible

--- VirtualReality/Program.cs
+++ VirtualReality/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
+using System.Collections.Generic;
 using System.Net.Sockets;
 using System.Threading;
 
@@ -12,20 +13,83 @@ namespace VirtualReality
 
         static void Main(string[] args)
         {
+            // Initialise and connect to the TcpClient
+            // On server: 145.48.6.10 and port: 6666
             TcpClient client = new TcpClient();
             client.Connect("145.48.6.10", 6666);
+
+            // Request the session list from the server
             string data = @"{""id"" : ""session/list""}";
-            byte[] byteData = System.Text.Encoding.ASCII.GetBytes(data);
-            int length = byteData.Length;
 
             NetworkStream networkStream = client.GetStream();           
-            networkStream.Write(BitConverter.GetBytes(length));
-            networkStream.Write(byteData);
 
-            
+            sendToTcp(networkStream, data);
+            // receive the response
+            string receivedData;
+            ReceiveFromTcp(networkStream, out receivedData);
 
+            // parse the received data
+            dynamic jsonData = JsonConvert.DeserializeObject(receivedData);
+            Console.WriteLine(jsonData.id);
+
+            JArray jsonDataArray = jsonData.data;
+            Console.WriteLine(jsonDataArray);
+
+            Dictionary<string, string> userSessionsMap = new Dictionary<string, string>();
+            foreach (JObject jObject in jsonDataArray)
+            {
+                if (jObject.ContainsKey("id") && jObject.ContainsKey("clientinfo"))
+                {
+                    string user = jObject["clientinfo"]["user"].ToString();
+                    Console.WriteLine(user);
+                    Console.WriteLine(jObject["id"]);
+                    // the dictionary doesn't like duplicates
+                    if (!userSessionsMap.ContainsKey(user))
+                    {
+                        userSessionsMap.Add(jObject["clientinfo"]["user"].ToString(), jObject["id"].ToString());
+                    }
+                    
+                }
+            }
+
+            // get user input for which session to connect to
+            Console.WriteLine("Which client should be connected to?");
+            string userInput = Console.ReadLine();
+            if (userSessionsMap.ContainsKey(userInput))
+            {
+                Console.WriteLine("Creating a tunnel");
+                // create a tunnel
+                string tunnelCreationDataString = @"{""id"" : ""tunnel/create"", 
+                                                        ""data"" : 
+                                                        {
+                                                            ""session"" : """ + userSessionsMap.GetValueOrDefault(userInput) + @""",
+                                                            ""key"" : """"
+                                                        }
+                                                    }";
+                sendToTcp(networkStream, tunnelCreationDataString);
+                string tunnelCreationResponse;
+                ReceiveFromTcp(networkStream, out tunnelCreationResponse);
+            } else
+            {
+                Console.WriteLine("That client isn't recognised");
+            }
+        }
+
+        public static void sendToTcp(NetworkStream networkStream, string data)
+        {
+            byte[] dataBytes = System.Text.Encoding.ASCII.GetBytes(data);
+            int dataLength = dataBytes.Length;
+
+            networkStream.Write(BitConverter.GetBytes(dataLength));
+            networkStream.Write(dataBytes);
+        }
+
+        public static void ReceiveFromTcp(NetworkStream networkStream, out string receivedData)
+        {
+            // read a small part of the packet and receive the packet length
             byte[] buffer = new byte[4];
             int rc = networkStream.Read(buffer, 0, 4);
+            // read from the stream untill the entire packet is written to the buffer
             int packetLength = BitConverter.ToInt32(buffer);
             byte[] packetBuffer = new byte[packetLength];
             int receivedTotal = 0;
@@ -34,13 +98,8 @@ namespace VirtualReality
                 rc = networkStream.Read(packetBuffer, receivedTotal, packetLength - receivedTotal);
                 receivedTotal += rc;
             }
-            string receivedData = System.Text.Encoding.ASCII.GetString(packetBuffer);
+            receivedData = System.Text.Encoding.ASCII.GetString(packetBuffer);
             Console.WriteLine(receivedData);
-
-            dynamic jsonData = JsonConvert.DeserializeObject(receivedData);
-            Console.WriteLine(jsonData.id);           
-
-            JArray jsonDataArray = jsonData.data;           
         }
     }
 }
