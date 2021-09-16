327bd3edcfe6bb48e9ddb40ae2add237635cf7c2
Robin <luijtenrobin@gmail.com>
2021-09-16 12:06:28 +0200

    Initial class setup for time change

---
+++ VirtualReality/TimeChange.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualReality
+{
+    class TimeChange
+    {
+        public float time { get {return time;} set {sendData(time);} }
+
+        public TimeChange(float time)
+        {
+            this.time = time;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="time"></param>
+        
+        public void sendData(float time)
+        {
+
+        }
+        public void sendData(bool staticTime)
+        {
+
+        }
+
+        public void SetStatic()
+        {
+            sendData(true);
+        }
+        
+    }
+}
