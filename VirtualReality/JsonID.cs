bd6f24fd99270c843a884c68186788f40f996b72
Jeroen <j.visser10@student.avans.nl>
2021-09-16 15:01:02 +0200

    Added JsonID and JsonObject classes.

---
+++ VirtualReality/JsonID.cs
@@ -0,0 +1,57 @@
+﻿namespace VirtualReality
+{
+    class JsonID
+    {
+        // Scene ID's
+        public static readonly string SCENE_GET = "scene/get";
+        public static readonly string SCENE_RESET = "scene/reset";
+        public static readonly string SCENE_SAVE = "scene/save";
+        public static readonly string SCENE_LOAD = "scene/load";
+        public static readonly string SCENE_RAYCAST = "scene/raycast";
+
+        // Scene/Node ID's
+        public static readonly string SCENE_NODE_ADD = "scene/node/add";
+        public static readonly string SCENE_NODE_MOVETO = "scene/node/moveto";
+        public static readonly string SCENE_NODE_UPDATE = "scene/node/update";
+        public static readonly string SCENE_NODE_DELETE = "scene/node/delete";
+        public static readonly string SCENE_NODE_ADDLAYER = "scene/node/addlayer";
+        public static readonly string SCENE_NODE_DELLAYER = "scene/node/dellayer";
+        public static readonly string SCENE_NODE_FIND = "scene/node/find";
+
+        // Scene/Terrain ID's
+        public static readonly string SCENE_TERRAIN_ADD = "scene/terrain/add";
+        public static readonly string SCENE_TERRAIN_UPDATE = "scene/terrain/update";
+        public static readonly string SCENE_TERRAIN_DELETE = "scene/terrain/delete";
+        public static readonly string SCENE_TERRAIN_GETHEIGHT = "scene/terrain/getheight";
+
+        // Scene/Panel ID's
+        public static readonly string SCENE_PANEL_CLEAR = "scene/panel/clear";
+        public static readonly string SCENE_PANEL_DRAWLINES = "scene/panel/drawlines";
+        public static readonly string SCENE_PANEL_DRAWTEXT = "scene/panel/drawtext";
+        public static readonly string SCENE_PANEL_IMAGE = "scene/panel/image";
+        public static readonly string SCENE_PANEL_SETCLEARCOLOR = "scene/panel/setclearcolor";
+        public static readonly string SCENE_PANEL_SAWP = "scene/panel/sawp";
+
+        // Scene/Skybox ID's
+        public static readonly string SCENE_SKYBOX_SETTIME = "scene/skybox/settime";
+        public static readonly string SCENE_SKYBOX_UPDATE = "scene/skybox/update";
+
+        // Scene/Road ID's
+        public static readonly string SCENE_ROAD_ADD = "scene/road/add";
+        public static readonly string SCENE_ROAD_UPDATE = "scene/road/update";
+
+        // Route ID's
+        public static readonly string ROUTE_ADD = "route/add";
+        public static readonly string ROUTE_UPDATE = "route/update";
+        public static readonly string ROUTE_DELETE = "route/delete";
+        public static readonly string ROUTE_FOLLOW = "route/follow";
+        public static readonly string ROUTE_FOLLOW_SPEED = "route/follow/speed";
+        public static readonly string ROUTE_SHOW = "route/show";
+
+        // Misc ID's
+        public static readonly string GET = "get";
+        public static readonly string SETCALLBACK = "setcallback";
+        public static readonly string PLAY = "play";
+        public static readonly string PAUSE = "pause";
+    }
+}
--- VirtualReality/JsonObject.cs
+++ VirtualReality/JsonObject.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,43 @@ namespace VirtualReality
 {
     class JsonObject
     {
-        
+		private delegate void dataCallback(dynamic data);
+		private static readonly Dictionary<string, dataCallback> callbacks = new();
 
-    }
+		/// <summary>Handle handles<c>the id and data that is being received.</c>It searches for the callback
+		/// in the dictionary and exeutes that callback with the given data.</summary>
+		///
 
+		public static void Handle(string id, dynamic data)
+		{
+			callbacks[id](data);
+		}
+
+		/// <summary>Send sends<c>the given id and data as a JSON object string.</c>The id and data get serialized into
+		/// an JSON object and than turned into a byte array. This byte array is than send to the server.</summary>
+		///
+
+		public static void Send(string id, dynamic data)
+		{
+			byte[] payload = System.Text.Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(new { id = id, data = data }));
+	        // TODO[Jeroen] Actualy send the payload.
+		}
+
+		/// <summary>Init does<c>the initialisation of all the callbacks that are used.</c>The callbacks are added to
+		/// a dictionary with the JSON object id as the key to easily process the data.</summary>
+		///
+
+		private static void Init()
+		{
+			callbacks["route/node/update"] = (data) =>
+			{
+				if (data.status != "ok")
+				{
+					// TODO[Jeroen] Add implementation.
+				}
+			};
+	        
+			// TODO[Jeroen] Implement the rest of the callbacks.
+		}
+	}
 }
