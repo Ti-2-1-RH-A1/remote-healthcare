1891db71f05244fab6f7ab237cc1995cf07b5a9d
Robin <luijtenrobin@gmail.com>
2021-09-20 14:01:03 +0200

    Documentation added, with time change it wil first set to dynamic

--- VirtualReality/TimeChange.cs
+++ VirtualReality/TimeChange.cs
@@ -18,19 +18,19 @@ namespace VirtualReality
         }
 
         /// <summary>
-        /// 
+        /// Change the <c>time</c> to the float specified
         /// </summary>
         /// <param name="time"></param>
-        
+
         public void sendData(float time)
         {
-            // create a tunnel
-            JObject tunnelCreateJson = new JObject { { "id", "scene/skybox/settime" } };
+            sendData(false);
+            JObject tunnelSetTimeJson = new JObject { { "id", "scene/skybox/settime" } };
 
             JObject dataJson = new JObject { { "time", time } };
 
-            tunnelCreateJson.Add("data", dataJson);
-            program.SendViaTunnel(tunnelCreateJson);
+            tunnelSetTimeJson.Add("data", dataJson);
+            program.SendViaTunnel(tunnelSetTimeJson);
             string tunnelCreationResponse = "";
 
            
@@ -40,47 +40,55 @@ namespace VirtualReality
             dynamic responseDeserializeObject = JsonConvert.DeserializeObject(tunnelCreationResponse);
             string response = responseDeserializeObject.ToString();
         }
-        public void sendData()
+
+        /// <summary>
+        /// Set the <c>skybox</c> to static 
+        /// </summary>
+        /// <param name="mode"></param>
+        public void sendData(bool mode)
         {
-             
+            if (mode)
+            {
+                JObject sendJson = new JObject();
+                sendJson.Add("id", "scene/skybox/update");
 
-            
+                JObject jsonData = new JObject();
+                jsonData.Add("type", "static");
 
-            //JObject sendJson = JObject.Parse(sendString);
+                JObject jsonFiles = new JObject();
+                jsonFiles.Add("xpos", @"data/NetworkEngine/textures/SkyBoxes/interstellar/interstellar_rt.png");
+                jsonFiles.Add("xneg", @"data/NetworkEngine/textures/SkyBoxes/interstellar/interstellar_lf.png");
+                jsonFiles.Add("ypos", @"data/NetworkEngine/textures/SkyBoxes/interstellar/interstellar_up.png");
+                jsonFiles.Add("yneg", @"data/NetworkEngine/textures/SkyBoxes/interstellar/interstellar_dn.png");
+                jsonFiles.Add("zpos", @"data/NetworkEngine/textures/SkyBoxes/interstellar/interstellar_bk.pn");
+                jsonFiles.Add("zneg", @"data/NetworkEngine/textures/SkyBoxes/interstellar/interstellar_ft.png");
 
-            JObject sendJson = new JObject();
-            sendJson.Add("id","scene/skybox/update");
+                jsonData.Add("files", jsonFiles);
+                sendJson.Add("data", jsonData);
 
-            JObject jsonData = new JObject();
-            jsonData.Add("type","static");
+                Console.WriteLine(sendJson);
+                program.SendViaTunnel(sendJson);
+                string tunnelCreationResponse = "";
+                program.ReceiveFromTcp(out tunnelCreationResponse);
+                Console.WriteLine(tunnelCreationResponse);
+            }
+            else
+            {
+                JObject sendJson = new JObject();
+                sendJson.Add("id", "scene/skybox/update");
 
-            JObject jsonFiles = new JObject();
-            jsonFiles.Add("xpos" , @"data/NetworkEngine/textures/SkyBoxes/interstellar/interstellar_rt.png");
-            jsonFiles.Add("xneg", @"data/NetworkEngine/textures/SkyBoxes/interstellar/interstellar_lf.png");
-            jsonFiles.Add("ypos", @"data/NetworkEngine/textures/SkyBoxes/interstellar/interstellar_up.png");
-            jsonFiles.Add("yneg", @"data/NetworkEngine/textures/SkyBoxes/interstellar/interstellar_dn.png");
-            jsonFiles.Add("zpos", @"data/NetworkEngine/textures/SkyBoxes/interstellar/interstellar_bk.pn");
-            jsonFiles.Add("zneg", @"data/NetworkEngine/textures/SkyBoxes/interstellar/interstellar_ft.png");
-            
-            jsonData.Add("files",jsonFiles);
-            sendJson.Add("data",jsonData);
+                JObject jsonData = new JObject();
+                jsonData.Add("type", "dynamic");
 
-            Console.WriteLine(sendJson);
+                sendJson.Add("data", jsonData);
 
-
-
-            program.SendViaTunnel(sendJson);
-            string tunnelCreationResponse = "";
-
-
-
-            program.ReceiveFromTcp(out tunnelCreationResponse);
-
-            Console.WriteLine(tunnelCreationResponse);
-
-            //dynamic responseDeserializeObject = JsonConvert.DeserializeObject(tunnelCreationResponse);
-            //string response = responseDeserializeObject["data"]["status"].ToString();
+                Console.WriteLine(sendJson);
+                program.SendViaTunnel(sendJson);
+                string tunnelCreationResponse = "";
+                program.ReceiveFromTcp(out tunnelCreationResponse);
+                Console.WriteLine(tunnelCreationResponse);
+            }
         }
-        
+
     }
 }
