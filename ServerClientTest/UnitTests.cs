7c2857eb5aeea3a4f4a53e0d20d9e5f6068814e8
Raoul de Heer <raouldeheer@users.noreply.github.com>
2021-09-29 15:57:22 +0200

    Added more unit tests

--- ServerClientTest/UnitTests.cs
+++ ServerClientTest/UnitTests.cs
@@ -1,13 +1,40 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ServerClient;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace ServerClientTest
+namespace ServerClient.Tests
 {
-    [TestClass]
+    [TestClass()]
     public class UnitTests
     {
+        [TestMethod()]
+        public void StringifyParseHeadersTest()
+        {
+            Dictionary<string, string> headers = new()
+            {
+                { "Test", "Header" },
+                { "Test2", "Header2" }
+            };
+            var stringHeader = Protocol.StringifyHeaders(headers);
+            var dictHeader = Protocol.ParseHeaders(stringHeader).Item1;
+            CollectionAssert.AreEqual(headers, dictHeader);
+        }
+
+        [TestMethod()]
+        public void StringifyParseDataTest()
+        {
+            Dictionary<string, string> data = new()
+            {
+                { "Test", "Data" },
+                { "Test2", "Data2" }
+            };
+            var stringData = Protocol.StringifyData(data);
+            var dictData = Protocol.ParseData(stringData);
+            CollectionAssert.AreEqual(data, dictData);
+        }
+
         [TestMethod]
         public async Task TestServerAsync()
         {
@@ -25,6 +52,37 @@ namespace ServerClientTest
         public void TestNoCertError()
         {
             Assert.ThrowsException<Exception>(() => Program.RunServer(@"Server.pfx"));
+        }
+
+        [TestMethod()]
+        public void ParsePacketTest()
+        {
+            Dictionary<string, string> headers = new()
+            {
+                { "Test", "Header" },
+                { "Test2", "Header2" }
+            };
+            Dictionary<string, string> data = new()
+            {
+                { "Test", "Data" },
+                { "Test2", "Data2" }
+            };
+            var packetString = $"{Protocol.StringifyHeaders(headers)}{Protocol.StringifyData(data)}";
+            var packetDicts = Protocol.ParsePacket(packetString);
+            CollectionAssert.AreEqual(headers, packetDicts.Item1);
+            CollectionAssert.AreEqual(data, packetDicts.Item2);
+        }
+
+        [TestMethod()]
+        public void ParseHeadersTest()
+        {
+            Assert.ThrowsException<ArgumentException>(() => Protocol.ParseHeaders("kdfkjfdkjfkddkfkdkjfkjdkjfkjdkfkjd"));
+        }
+
+        [TestMethod()]
+        public void ParseDataTest()
+        {
+            Assert.ThrowsException<ArgumentException>(() => Protocol.ParseData("kdfkjfdkjfkddkfkdkjfkjdkjfkjdkfkjd"));
         }
     }
 }
