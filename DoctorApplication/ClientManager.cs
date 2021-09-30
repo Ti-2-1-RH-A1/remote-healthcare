ecee6ba38b77f44b6a152af7b75f7601962add58
Robin <luijtenrobin@gmail.com>
2021-09-30 10:40:11 +0200

    Doctor GUI changes

---
+++ DoctorApplication/ClientManager.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Windows.Input;
+
+namespace DoctorApplication
+{
+    class ClientManager 
+    {
+
+        public ClientManager()
+        {
+        }
+
+        
+    }
+}
