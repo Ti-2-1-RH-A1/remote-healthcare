64494c935ef4f398c7539b1cd87026157dfb52eb
JustRostin <luijtenrobin@gmail.com>
2021-10-06 14:38:06 +0200

    Apply suggestions from code review
    Co-authored-by: Raoul de Heer <raouldeheer@users.noreply.github.com>

--- ServerClient/Program.cs
+++ ServerClient/Program.cs
@@ -10,8 +10,17 @@ namespace ServerClient
             string certificate = @"Server.pfx";
             new Server(certificate, new AuthHandler(), true);
 
-            
+
+var client = new Client("localhost", "Fiets", true);
             await Task.Delay(3000);
+
+            // client.SendPacket(new Dictionary<string, string>() {
+            //     { "Method", "Get" }
+            // }, new Dictionary<string, string>(), (header, data) =>
+            // {
+            //     Console.WriteLine(e1);
+            //     Console.WriteLine(e2);
+            // });
 
             Console.ReadLine();
         }
