683b92e944278cc76025feff2dfd5b01b15b7bdf
Robin <luijtenrobin@gmail.com>
2021-10-06 14:43:43 +0200

    Comments ffout in commit

--- ServerClient/Program.cs
+++ ServerClient/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace ServerClient
@@ -8,15 +9,16 @@ namespace ServerClient
         public static async Task Main(string[] args)
         {
             string certificate = @"Server.pfx";
-            new Server(certificate, AuthHandler.Init(), true);
+            new Server(certificate, new AuthHandler(), false);
+
+            var client = new Client("localhost", "Fiets", false);
 
 
-var client = new Client("localhost", "Fiets", true);
             await Task.Delay(3000);
 
             // client.SendPacket(new Dictionary<string, string>() {
             //     { "Method", "Get" }
-            // }, new Dictionary<string, string>(), (header, data) =>
+            // }, new Dictionary<string, string>(), (e1, e2) =>
             // {
             //     Console.WriteLine(e1);
             //     Console.WriteLine(e2);
