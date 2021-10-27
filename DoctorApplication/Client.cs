c49e78f7e7471ee8cb2e01ce9834a8b48ddabc70
Raoul de Heer <raouldeheer@users.noreply.github.com>
2021-10-27 10:59:25 +0200

    save rtdata to client

--- DoctorApplication/Client.cs
+++ DoctorApplication/Client.cs
@@ -5,6 +5,10 @@
         public string clientAuthKey { get; set; }
         public string clientName { get; set; }
         public string clientSerial { get; set; }
-
+        public string speed { get; set; }
+        public string time { get; set; }
+        public string distance_traveled { get; set; }
+        public string rpm { get; set; }
+        public string heartrate { get; set; }
     }
 }
